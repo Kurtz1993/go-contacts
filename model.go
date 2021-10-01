afb24aafeb050d263f26069be2e54b8c1d2bdc03
Luis Hernandez <luishernandez@datawire.io>
2021-10-01 13:03:42 -0500

    Define skeleton

--- model.go
+++ model.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var ErrNotImplemented = errors.New("not implemented")
+
+type Contact struct {
+	ID          int    `json:"id"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName,omitempty"`
+	PhoneNumber string `json:"phoneNumber,omitempty"`
+	Email       string `json:"email,omitempty"`
+}
+
+func (c *Contact) queryContact(db *sql.DB) error {
+	return ErrNotImplemented
+}
+
+func (c *Contact) updateContact(db *sql.DB) error {
+	return ErrNotImplemented
+}
+
+func (c *Contact) deleteContact(db *sql.DB) error {
+	return ErrNotImplemented
+}
+
+func (c *Contact) createContact(db *sql.DB) error {
+	return ErrNotImplemented
+}
+
+func listContacts(db *sql.DB, start, count int) ([]Contact, error) {
+	return nil, ErrNotImplemented
+}

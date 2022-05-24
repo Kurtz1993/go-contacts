37edd96100523183381028fcc920623f1ab91a83
Luis Hernandez <luis.hernandez@misalud.ai>
2022-05-24 11:24:10 -0500

    Add missing key

--- src/views/ContactsView.tsx
+++ src/views/ContactsView.tsx
@@ -1,8 +1,8 @@
-import ContactCard from "@components/ContactCard";
-import { AsyncData, AsyncStatus } from "@models/async-data.model";
-import { Contact } from "@models/contact.model";
-import { getContacts } from "@services/contactsApi";
-import { useEffect, useState } from "react";
+import ContactCard from '@components/ContactCard';
+import { AsyncData, AsyncStatus } from '@models/async-data.model';
+import { Contact } from '@models/contact.model';
+import { getContacts } from '@services/contactsApi';
+import { useEffect, useState } from 'react';
 
 export default function ContactsView() {
   const [contacts, setContacts] = useState<AsyncData<Contact[]>>({
@@ -13,10 +13,10 @@ export default function ContactsView() {
   useEffect(() => {
     if (contacts.status === AsyncStatus.Idle) {
       getContacts()
-        .then((data) => {
+        .then(data => {
           setContacts({ status: AsyncStatus.Fulfilled, data });
         })
-        .catch((err) => console.log(err));
+        .catch(err => console.log(err));
     }
   }, [contacts.status]);
 
@@ -24,8 +24,8 @@ export default function ContactsView() {
     <div>
       <h1 className="text-center text-5xl mb-10">Contacts Book</h1>
       <div className="flex flex-wrap justify-center">
-        {contacts.data.map((item) => (
-          <ContactCard contact={item} />
+        {contacts.data?.map(item => (
+          <ContactCard contact={item} key={item.id} />
         ))}
       </div>
     </div>

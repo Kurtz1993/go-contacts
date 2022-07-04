f531fd4cfdab914e6db869c440a5e47a83ad8fc3
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2022-07-03 21:22:44 -0500

    Port to vite

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+import svgr from '@honkhonk/vite-plugin-svgr;';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [tsconfigPaths(), react(), checker({ typescript: true }), svgr()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      reporter: ['text', 'html'],
+      exclude: ['node_modules/', 'src/setupTests.ts'],
+    },
+  },
+});

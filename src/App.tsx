5a7023395971f9b241405ffb2eb7673a7ae52f26
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2022-07-03 23:46:10 -0500

    Add provider and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import store from './redux/store';
+import AddContactView from './views/AddContactView';
 import ContactsView from './views/ContactsView';
 
 function App() {
   return (
-    <main className="flex flex-col h-screen bg-gray-200 p-6">
-      <ContactsView />
-    </main>
+    <Provider store={store}>
+      <main className="flex flex-col h-screen bg-gray-200 p-6">
+        <Router>
+          <Routes>
+            <Route path="/" element={<ContactsView />} />
+            <Route path="/add" element={<AddContactView />} />
+          </Routes>
+        </Router>
+      </main>
+    </Provider>
   );
 }
 

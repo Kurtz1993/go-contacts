a9f51b08be486d9f572d18f927115ff786f08548
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2022-07-04 00:05:03 -0500

    Implement mutation resolver

--- internal/graph/schema.resolvers.go
+++ internal/graph/schema.resolvers.go
@@ -5,18 +5,34 @@ package graph
 
 import (
 	"context"
+	"crypto/md5"
+	"fmt"
 
 	"github.com/Kurtz1993/go-contacts/internal/graph/generated"
 	"github.com/Kurtz1993/go-contacts/internal/graph/model"
+	"github.com/Kurtz1993/go-contacts/internal/pkg/datastore"
 )
 
 // CreateContact is the resolver for the createContact field.
 func (r *mutationResolver) CreateContact(ctx context.Context, input model.NewContact) (*model.Contact, error) {
-	return &model.Contact{
+	result, err := r.contacts.CreateContact(&datastore.ContactEntity{
 		FirstName:   input.FirstName,
 		LastName:    input.LastName,
-		PhoneNumber: input.PhoneNumber,
-		Email:       input.Email,
+		PhoneNumber: *input.PhoneNumber,
+		Email:       *input.Email,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	avatarURL := fmt.Sprintf("https://www.gravatar.com/avatar/%x", md5.Sum([]byte(result.Email)))
+	return &model.Contact{
+		ID:          result.ID,
+		AvatarURL:   &avatarURL,
+		FirstName:   result.FirstName,
+		LastName:    result.LastName,
+		PhoneNumber: &result.PhoneNumber,
+		Email:       &result.Email,
 	}, nil
 }
 
@@ -29,11 +45,13 @@ func (r *queryResolver) Contacts(ctx context.Context) ([]*model.Contact, error) 
 
 	var result []*model.Contact
 	for _, item := range data {
+		avatarURL := fmt.Sprintf("https://www.gravatar.com/avatar/%x", md5.Sum([]byte(item.Email)))
 		result = append(result, &model.Contact{
 			ID:          item.ID,
 			FirstName:   item.FirstName,
 			LastName:    item.LastName,
 			PhoneNumber: &item.PhoneNumber,
+			AvatarURL:   &avatarURL,
 			Email:       &item.Email,
 		})
 	}

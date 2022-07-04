b945ab920740a5c91aa6fc2909a4d97ab00ec54e
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2022-07-03 23:14:11 -0500

    Set store

---
+++ src/redux/store.ts
@@ -0,0 +1,18 @@
+import { api } from '@app/api/baseApi';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({
+  [api.reducerPath]: api.reducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware().concat([api.middleware]);
+  },
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;

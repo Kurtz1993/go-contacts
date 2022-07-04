0c98d68e62e44ad104238b0f60d6a558795d842b
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2022-07-03 23:46:27 -0500

    Export store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -12,6 +12,7 @@ const store = configureStore({
   },
 });
 
+export default store;
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}

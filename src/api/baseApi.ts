ae007d3af7d3512c526131fec974f1c21d6833da
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2022-07-03 23:14:43 -0500

    Add base api

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi } from '@reduxjs/toolkit/query/react';
 import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query';
 import { GraphQLClient } from 'graphql-request';
 
-export const client = new GraphQLClient('/graphql');
+export const client = new GraphQLClient('http://localhost:8080/graphql');
 // highlight-start
 export const api = createApi({
   baseQuery: graphqlRequestBaseQuery({ client }),

2a2d80eb1d515b1aed448f5ed7c169063e613e2e
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2022-07-03 23:54:18 -0500

    Add dependency on contats dao

--- internal/graph/resolver.go
+++ internal/graph/resolver.go
@@ -1,8 +1,12 @@
 package graph
 
+import "github.com/Kurtz1993/go-contacts/internal/pkg/datastore"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 //go:generate go run github.com/99designs/gqlgen
-type Resolver struct{}
+type Resolver struct {
+	contacts datastore.ContactsDAO
+}

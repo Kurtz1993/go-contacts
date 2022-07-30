2605fcf74b022a7e2ebb66b9c1b62e52641bec26
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2022-07-30 12:56:27 -0500

    Add ios capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.contacts',
+  appName: 'Contacts',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;

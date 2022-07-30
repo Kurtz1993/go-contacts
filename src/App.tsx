53aaaf636dcc2a393d13194ae2d7390295dd0c72
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2022-07-30 12:57:58 -0500

    Use IonApp container

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import { QueryClientProvider } from '@tanstack/react-query';
+import { IonApp } from '@ionic/react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { queryClient } from './config/queryClient';
 import AddContactView from './views/AddContactView';
 import ContactsView from './views/ContactsView';
 import UpdateContactView from './views/UpdateContactView';
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <main className="flex flex-col h-screen bg-gray-200 p-6">
-        <Router>
-          <Routes>
-            <Route path="/" element={<ContactsView />} />
-            <Route path="/add" element={<AddContactView />} />
-            <Route path="/edit/:id" element={<UpdateContactView />} />
-          </Routes>
-        </Router>
-      </main>
+    <IonApp>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ContactsView />} />
+          <Route path="/add" element={<AddContactView />} />
+          <Route path="/edit/:id" element={<UpdateContactView />} />
+        </Routes>
+      </Router>
+
       <ReactQueryDevtools />
-    </QueryClientProvider>
+    </IonApp>
   );
 }
 

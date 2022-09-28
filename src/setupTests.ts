07d630e38b838d23d321879ae8b5e68d5bd28978
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2022-09-28 15:55:12 -0500

    Add msw support

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -5,4 +5,13 @@
 import '@testing-library/jest-dom';
 import fetch from 'node-fetch';
 
+import { testServer } from './mocks/testServer';
+// Establish API mocking before all tests.
+beforeAll(() => testServer.listen());
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => testServer.resetHandlers());
+// Clean up after the tests are finished.
+afterAll(() => testServer.close());
+
 global.fetch = fetch as any;

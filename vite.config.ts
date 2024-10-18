e09e8f68f4c80594edc433a571065af96f631765
Luis Hernández <luis.hlopez@outlook.com>
2024-10-18 13:48:38 -0600

    chore: move src to web

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './web/src/setupTests.ts',
+    setupFiles: './web/setupTests.ts',
   },
   server: {
     hmr: {
